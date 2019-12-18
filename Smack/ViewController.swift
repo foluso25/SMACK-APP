f75c3fe069db338f3c2249900e1761ec1b52461c
foluso <foluso.akinlade@gmail.com>
2019-12-18 00:03:57 -0500

    Initial Commit

---
+++ Smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Admin on 12/18/19.
+//  Copyright © 2019 Foluso's. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
